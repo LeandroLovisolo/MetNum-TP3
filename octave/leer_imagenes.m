780769dc6203acc2ad80ed24ed24cc87dc41a79d
Leandro Lovisolo <leandro@leandro.me>
2013-06-03 18:01:24 -0300

    Prints y comentarios

--- octave/leer_imagenes.m
+++ octave/leer_imagenes.m
@@ -1,11 +1,13 @@
 function Xs = leer_imagenes(images_path, labels_path)
 
-images = leerMNISTimage(images_path);
-labels = leerMNISTlabel(labels_path);
+images = leerMNISTimage(images_path); %(:,:,1:1000);
+labels = leerMNISTlabel(labels_path); %(    1:1000,:);
 
 Xs = {};
 
 for i = 0:9
+    printf('Filtrando dígito %d...\n', i);
+    fflush(stdout);
     X = filtrar_por_digito(images, labels, i);
     Xs{end + 1} = X;
 end
