406abb5d975fb7647a820be018b39f41861e555e
Lautaro Petaccio <lausuper@gmail.com>
2013-06-08 16:01:51 -0300

    Cambiado leer_imagenes para que no las separe

--- octave/leer_imagenes.m
+++ octave/leer_imagenes.m
@@ -1,11 +1,18 @@
-function Xs = leer_imagenes(images_path, labels_path)
-
+function [Img, Lab] = leer_imagenes(images_path, labels_path)
 images = leerMNISTimage(images_path);
-labels = leerMNISTlabel(labels_path);
-
-Xs = {};
-
-for i = 0:9
-    X = filtrar_por_digito(images, labels, i);
-    Xs{end + 1} = X;
-end
+printf('Imagenes leidas')
+Lab = leerMNISTlabel(labels_path);
+printf('Labels leidas')
+Img = [];
+for i = 1:size(images)(3)
+	imgrow = images(:,:,i)'(:)';
+    if(length(Img) == 0)
+		Img = imgrow;
+	else
+		Img = [Img; imgrow];
+	endif
+endfor
+mu = mean(Img);
+size(Img)
+n = size(X)(1);
+Img = (Img - mu) / sqrt(n - 1);
