7382cd40155c4d1edd47966c5c97d2754dd99aaa
Lautaro Petaccio <lausuper@gmail.com>
2013-06-09 15:45:42 -0300

    Corregida división en matrizDeConvarianza

--- ecuaciones.cpp
+++ ecuaciones.cpp
@@ -1,8 +1,9 @@
 #include "Matriz.h"
+#include <cmath>
 
 Matriz* matrizDeCovarianza(Matriz& A) {
 	Matriz *X = A.transformarAMediaCero();
-	Matriz *Xret = (*X)*(1/(X->filas()-1));
+	Matriz *Xret = (*X)*(1/sqrt(X->filas()-1));
 	delete X;
 	return Xret;
 }
