707d45a213fa27cf742b5d9318f507031aa17d9f
Lautaro Petaccio <lausuper@gmail.com>
2013-06-11 11:47:22 -0300

    Funcion TC

--- ecuaciones.cpp
+++ ecuaciones.cpp
@@ -1,9 +1,17 @@
 #include "Matriz.h"
+#include <iostream>
 #include <cmath>
-
+using namespace std;
 Matriz* matrizDeCovarianza(Matriz& A) {
 	Matriz *X = A.transformarAMediaCero();
 	Matriz *Xret = (*X)*(1/sqrt(X->filas()-1));
 	delete X;
 	return Xret;
+}
+
+Matriz* TC(Matriz& x, Matriz&Vt) {
+	//Transponemos ya que tenemos las imágenes como filas
+	//Cuidado que transponer modifica x
+	x.transponer();
+	return Vt*x;
 }
