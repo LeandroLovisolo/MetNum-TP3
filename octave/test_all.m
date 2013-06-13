747aae086090a3c37951b73151ab17a352d80114
Leandro Lovisolo <leandro@leandro.me>
2013-06-12 23:26:50 -0300

    Limpieza del test global

--- octave/test_all.m
+++ octave/test_all.m
@@ -1,6 +1,6 @@
 function test_all()
 
-num_images = 1000;
+num_images = 4000;
 num_tests = 500;
 k = 10;
 
@@ -13,29 +13,16 @@ msg('Filtrando imágenes de entrenamiento por dígito...\n');
 
 imgs = {[], [], [], [], [], [], [], [], [], []};
 
-% for i = 1:num_images
-%     imgs{labels(i) + 1}(end + 1, :) = images(:, :, i)'(:)';
-% end
-
-i = j = 1;
-while(j <= num_images)
-    if(labels(i) == 0); imgs{1}(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    if(labels(i) == 5); imgs{6}(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    if(labels(i) == 8); imgs{9}(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    i++;
+for i = 1:num_images
+    imgs{labels(i) + 1}(end + 1, :) = images(:, :, i)'(:)';
 end
-
-msg(cstrcat(num2str(rows(imgs{1})), ' imágenes dígito 0.\n'));
-msg(cstrcat(num2str(rows(imgs{6})), ' imágenes dígito 5.\n'));
-msg(cstrcat(num2str(rows(imgs{9})), ' imágenes dígito 8.\n'));
 
 msg('Armando matriz X...\n');
 
-% X = [];
-% for(i = 1:10)
-%     X = [X; imgs{i}];
-% end
-X = [imgs{1}; imgs{6}; imgs{9}];
+X = [];
+for(i = 1:10)
+    X = [X; imgs{i}];
+end
 X = double(X);
 
 mu = mean(X);
@@ -65,75 +52,20 @@ for i = 1:10
     medias(end + 1,:) = mean(Ts{i});
 end
 
-medias
-
 msg('Cargando imágenes de prueba...\n');
 
 images = leerMNISTimage('../t10k-images-idx3-ubyte');
 labels = leerMNISTlabel('../t10k-labels-idx1-ubyte');
 
-msg('Filtrando imágenes de prueba por dígito...\n');
-
-test0 = [];
-test5 = [];
-test8 = [];
-
-i = j = 1;
-while(j <= num_tests)
-    if(labels(i) == 0); test0(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    if(labels(i) == 5); test5(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    if(labels(i) == 8); test8(end + 1, :) = images(:,:,i)'(:)'; j++; endif
-    i++;
-end
-
 msg('Calculando tasa de aciertos...\n');
 
 aciertos = 0;
 
-msg('\nImágenes de prueba dígito 0\n\n');
-
-for i=1:rows(test0)
-    d = reconocer_digito(V, medias, k, test0(i,:));
-    msg(cstrcat('Dígito reconocido: ', num2str(d), '\n'));
-    if(d == 0);
-        msg('Acierto!\n');
+for i = 1:num_tests
+    if(reconocer_digito(V, medias, k, images(:,:,i)'(:)') == labels(i))
         aciertos++;
     endif
 end
-
-msg('\nImágenes de prueba dígito 5\n\n');
-
-for i=1:rows(test5)
-    d = reconocer_digito(V, medias, k, test5(i,:));
-    msg(cstrcat('Dígito reconocido: ', num2str(d), '\n'));
-    if(d == 5);
-        msg('Acierto!\n');
-        aciertos++;
-    endif
-end
-
-msg('\nImágenes de prueba dígito 8\n\n');
-
-for i=1:rows(test8)
-    d = reconocer_digito(V, medias, k, test8(i,:));
-    msg(cstrcat('Dígito reconocido: ', num2str(d), '\n'));
-    if(d == 8);
-        msg('Acierto!\n');
-        aciertos++;
-    endif
-end
-
-% i = j = 1
-% while(j <= num_tests)
-%     if(labels(i) == 0 || labels(i) == 5 || labels(i) == 9)
-%         j++;
-%         if(reconocer_digito(V, medias, k, images(:,:,i)'(:)') == labels(i))
-%             msg('Acierto!\n');
-%             aciertos++;
-%         endif
-%     endif
-%     i++;
-% end
 
 printf('Tests: %d\n', num_tests);
 printf('Aciertos: %d\n', aciertos);
@@ -154,20 +86,9 @@ D = zeros(1, 10);
 for i = 1:10
     D(i) = norm(T - medias(i,:));
 end
-%[x, idx] = min(D);
-[x, idx] = min([D(1) D(6) D(9)]);
 
-if(idx == 1)
-    d = 0;
-endif
-if(idx == 2)
-    d = 5;
-endif
-if(idx == 3)
-    d = 8;
-endif
-
-%d = idx - 1;
+[x, idx] = min(D);
+d = idx - 1;
 
 
 function msg(m)
